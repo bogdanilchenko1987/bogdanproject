16d989c6c47f47137666247881de63d1396161d8
Bogdan <example@gmail.com>
2020-09-22 00:25:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const answers = [];
+answers[0] = prompt('Kak vashe imya?','');
+answers[1] = prompt('Kak vashe familia?','');
+answers[2] = prompt('Skolko let?','');
+
+document.write(answers);
